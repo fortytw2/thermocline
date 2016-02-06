49ca26256720aac0d66cb7c3b7fe802b69b58c89
Ian C <fortytw2@gmail.com>
2016-02-06 15:24:01 -0500

    fix travis

--- brokers/brokers_test.go
+++ brokers/brokers_test.go
@@ -1,8 +1,14 @@
 package brokers_test
 
 import (
+	"testing"
+
 	"github.com/fortytw2/thermocline"
 	"github.com/fortytw2/thermocline/brokers/mem"
 )
 
 var brokers = []thermocline.Broker{mem.NewBroker()}
+
+func TestBrokers(t *testing.T) {
+
+}
